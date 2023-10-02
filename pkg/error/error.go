11cb0e93b89c4866922cdc5234b50404ab5a4d47
Beomgi Kim <chromato99@gmail.com>
2023-10-02 11:21:03 +0000

    add integrity check when sending file

--- pkg/error/error.go
+++ pkg/error/error.go
@@ -1,7 +1,15 @@
 package error
 
+import "errors"
+
 const (
 	ConnectionClosedByPeer = "Application error 0x0 (remote): Connection closed by peer"
 
 	NoRecentActivity = "timeout: no recent network activity"
+
+	FileModifiedDuringTransferCode = 0x1
+)
+
+var (
+	ErrFileModifiedDuringTransfer = errors.New("file modified during transfer")
 )
